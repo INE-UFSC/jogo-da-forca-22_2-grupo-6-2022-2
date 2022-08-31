40315efaec35d6170548df9f89a3b6f2b24aa298
jresmer <jpresmer@gmail.com>
2022-08-31 09:21:34 -0300

    func formadormatriz

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 continuar_jogo = True
-while continuar_jogo:
+
+def formadormatriz(frase, numero_de_caracteres):
     matriz_mapeamento = ["no"]*26
+    for letra_pos in range (numero_de_caracteres):
+        valor_ascii = ord(frase[letra_pos].lower()) - 97
+        if frase[letra_pos] == " ":
+            continue
+        elif matriz_mapeamento[valor_ascii] == "no":
+            matriz_mapeamento[valor_ascii] = [letra_pos]
+        else:
+            matriz_mapeamento[valor_ascii] += [letra_pos]
+    return matriz_mapeamento
+
+while continuar_jogo:
     lista_de_frases = ["bola de bilhar", "orientacao a objetos", "manipulacao dos dados", "projeto final", "spaghetti code", "palavra", "teste", "debug", "correcao"]
     opcao = input("digite '1' para adicionar uma frase ou palavra a lista ou '2' para continuar:")
     if opcao == '1':
@@ -11,17 +23,11 @@ while continuar_jogo:
             continuar_checagem = input("digite '1' para continuar a adicionar plavras/frases ou '2' para encerrar esse processo")
             if continuar_checagem == '2':
                 continuar = False
-    frase = input("digite um número entre 0 e %d para selecionar a palavra/frase." % len(lista_de_frases))
+    frase = input("digite um número entre 0 e %d para selecionar a palavra/frase." % (len(lista_de_frases)-1))
     frase = lista_de_frases[int(frase)]
     numero_de_caracteres = len(frase)
-    for letra_pos in range (numero_de_caracteres):
-        valor_ascii = ord(frase[letra_pos].lower()) - 97
-        if frase[letra_pos] == " ":
-            continue
-        elif matriz_mapeamento[valor_ascii] == "no":
-            matriz_mapeamento[valor_ascii] = [letra_pos]
-        else:
-            matriz_mapeamento[valor_ascii] += [letra_pos]
+    matriz_mapeamento = formadormatriz(frase, numero_de_caracteres)
     continuar_jogo_checagem = input("digite '1' para encerrar o jogo ou '2' para continuar para a próxima rodada:")
     if continuar_jogo_checagem == '1':
         continuar_jogo = False
+
