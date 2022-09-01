76fe768befd9b42f66e3f77e544ec71501c9167d
Guilherme De Jesus <guilhermeadenilson16@gmail.com>
2022-09-01 19:45:25 -0300

    Ajustes na função

--- tentativas.py
+++ tentativas.py
@@ -1,13 +1,23 @@
 def tentativas(numero_de_caracteres,frase,matriz_mapeamento):
     erros = 0
     casas_letras = ["_"]*numero_de_caracteres
-    letras_tentadas = []
+    letras_tentadas = set()
 
     for a in range(len(frase)):
         if frase[a] == " ":
             casas_letras[a] = " "
 
     while True:
+        letras_tentadas = set(letras_tentadas)
+        letras_tentadas = list(letras_tentadas)
+        #print(forca(erros))
+        for i in casas_letras:
+            print(i, end = '')
+        print('\nLetras tentadas: ', end = '')
+        for a in letras_tentadas:
+            print(a, end = ' ')
+        print()
+
         letra = input()
         valor_ascii = ord(letra.lower())-97
     
@@ -18,15 +28,15 @@ def tentativas(numero_de_caracteres,frase,matriz_mapeamento):
         if matriz_mapeamento[valor_ascii] != "no":
            for i in matriz_mapeamento[valor_ascii]:
                casas_letras[i] = chr(valor_ascii+97)
+               letras_tentadas.append(chr(valor_ascii+97))
         else:
             erros += 1
             letras_tentadas.append(chr(valor_ascii+97))
-            print(letras_tentadas)
         
         if erros == 6:
-            print("Você perdeu!")
+            #print(forca(erros))
             break
         
         elif "_" not in casas_letras:
-            print("Acertou :D")
+            #print(vitoria())
             break
