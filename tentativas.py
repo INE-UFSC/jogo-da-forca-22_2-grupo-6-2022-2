a148d7b31a0b5033edd59503b1e656e4aef73889
Guilherme De Jesus <guilhermeadenilson16@gmail.com>
2022-09-01 22:32:35 -0300

    Transferindo função jogo para tentativas

--- tentativas.py
+++ tentativas.py
@@ -2,6 +2,52 @@
 
 from main import jogo
 
+def jogo():
+    lista_de_palavras = open("palavras.txt", "r")
+    lista_de_palavras = lista_de_palavras.read().splitlines()
+    frase = random.choice(lista_de_palavras)
+    numero_de_caracteres = len(frase)
+    matriz_mapeamento = formadormatriz(frase, numero_de_caracteres)
+    erros = 0
+    casas_letras = ["_"] * numero_de_caracteres
+    letras_tentadas = []
+           
+    while True:
+        print(casas_letras)
+        print("Digite uma letra:")
+        print(matriz_mapeamento)
+        letra = input()
+        valor_ascii = ord(letra.lower())-97
+        while valor_ascii > 26 or valor_ascii < 0:
+            letra = input()
+            valor_ascii = ord(letra.lower())-97
+
+        for letra in range(len(frase)):
+            if letra in matriz_mapeamento[valor_ascii]:
+                casas_letras[letra] = chr(valor_ascii+97)
+            else:
+                pass
+
+        if matriz_mapeamento[valor_ascii] != "no":
+            for i in matriz_mapeamento[valor_ascii]:
+                print(chr(valor_ascii+97), end = " ")
+                draw_stage(erros)
+        else:
+            erros += 1
+            if erros <= 5:
+                print("Letra errada!")
+                draw_stage(erros)
+        if erros == 6:
+            print("Você perdeu!")
+            draw_stage(6)
+            menu()
+            break
+        elif "_" not in casas_letras:
+            print("Acertou :D")
+            draw_stage(7)
+            menu()
+            break
+
 def tentativas(numero_de_caracteres,frase,matriz_mapeamento):
     erros = 0
     casas_letras = ["_"]*numero_de_caracteres
