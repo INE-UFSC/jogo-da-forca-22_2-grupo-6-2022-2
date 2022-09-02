cc8e141ad7430ea58ff17910bbada9f1180953ed
Ayeloo <augustomborsoi@gmail.com>
2022-09-01 22:28:34 -0300

    Ordem do ASCII art

--- draw.py
+++ draw.py
@@ -2,29 +2,29 @@
 # Recebe como parâmetro total de erros e printa a cada input do usuário o "bonequinho" do estágio atual (Feito)
 
 '''  Lista de desenhos com índice relativo a quantidade de erros atual do round:
-     erros: 0 Inteiro
-     erros: 1 Braço Esq
-     erros: 2 Braço Dir
-     erros: 3 Perna Esq 
-     erros: 4 Perna Dir
-     erros: 5 Corpo
+     erros: 0 nada
+     erros: 1 cabeça
+     erros: 2 corpo
+     erros: 3 Perna esq
+     erros: 4 Perna dir
+     erros: 5 braço esq
      erros: 6 Game Over '''
 
 stage_list = ['''
      _______
      |/      |
-     |      (_)
-     |      \|/
-     |       |
-     |      / \\
+     |
+     |
+     |
+     |
      |
     _|___''', '''
     _______
      |/      |
      |      (_)
-     |       |/
-     |       |
-     |      / \\
+     |
+     |
+     |
      |
     _|___''', '''
     _______
@@ -32,7 +32,7 @@ stage_list = ['''
      |      (_)
      |       |
      |       |
-     |      / \\
+     |
      |
     _|___''' , '''
     _______
@@ -48,22 +48,22 @@ stage_list = ['''
      |      (_)
      |       |
      |       |
-     |       
+     |      / \\
      |
     _|___''' , '''
     _______
      |/      |
      |      (_)
-     |       
-     |       
-     |      
+     |       |\
+     |       |
+     |      / \\
      |
-    _|___''', '''                                                                      
-  ______  ____    ____    __  ______   _____  __    _ ______  _____   
- |   ___||    \  |    \  /  ||   ___| /     \\\\  \  //|   ___||     |  
- |   |  ||     \ |     \/   ||   ___| |     | \  \// |   ___||     \  
- |______||__|\__\|__/\__/|__||______| \_____/  \__/  |______||__|\__\                                                                      
-                                                                      
+    _|___''', '''
+  ______  ____    ____    __  ______   _____  __    _ ______  _____
+ |   ___||    \  |    \  /  ||   ___| /     \\\\  \  //|   ___||     |
+ |   |  ||     \ |     \/   ||   ___| |     | \  \// |   ___||     \
+ |______||__|\__\|__/\__/|__||______| \_____/  \__/  |______||__|\__\
+
 ''', '''ganho uhuu''']
 
 def draw_stage(erros):
